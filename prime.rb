a2435f1bffd3cf0b86231079c8b01f38a9d3ffb9
ode-to-the-code <justcallmethis@gmail.com>
2017-05-13 21:08:29 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,25 @@
 # Add  code here!
+
+
+def prime?(integer)
+  # you can loop downward, testing modulo till 1. if theres evidence that it could be
+  #divided by more numbers than 1 or itself, then return false
+  i = integer
+  div_inc = 0
+
+  while i > 0
+    if integer % i == 0
+      div_inc += 1
+    end
+    i -= 1
+  end
+
+  if div_inc > 2
+   false
+ elsif integer <= 1
+   false
+  else
+    true
+  end
+
+end
